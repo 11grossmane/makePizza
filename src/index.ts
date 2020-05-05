2177adc64ca1fb6dc19d66f7d564fb0d55ef2b83
Eric Grossman <eric.josh.grossman@gmail.com>
2020-05-05 15:30:41 -0400

    sdsf

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
 #!/usr/bin/env node
-import chalk = require('chalk')
-import clear = require('clear')
-import figlet = require('figlet')
-import path = require('path')
-import program from 'commander'
-import { gimme } from 'gimme-state'
+import chalk = require("chalk");
+import clear = require("clear");
+import figlet = require("figlet");
+import path = require("path");
+import program from "commander";
+import { gimme } from "gimme-state";
 
-clear()
+clear();
 console.log(
-    chalk.red(figlet.textSync('pizza-cli', { horizontalLayout: 'full' }))
-)
+  chalk.red(figlet.textSync("pizza-cli", { horizontalLayout: "full" }))
+);
 
 program
-    .version('0.0.1')
-    .description('example cli for ordering pizza')
-    .option('-p, --peppers', 'Add peppers')
-    .option(
-        '-c, --cheese <type>',
-        'Add the specified type of cheese [marble, colby]'
-    )
-    .parse(process.argv)
+  .version("0.0.1")
+  .description("example cli for ordering pizza")
+  .option("-p, --peppers", "Add peppers")
+  .option(
+    "-c, --cheese <type>",
+    "Add the specified type of cheese [marble, colby]"
+  )
+  .parse(process.argv);
 
-console.log('you ordered a pizza with:')
-if (program.peppers) console.log('  - peppers')
+console.log("you ordered a pizza with:");
+if (program.peppers) console.log("  - peppers");
 if (program.cheese) {
-    console.log('  - %s cheese', program.cheese)
+  console.log("  - %s cheese", program.cheese);
 } else {
-    console.log('  - no cheese')
+  console.log("  - no cheese");
 }
 
 if (!process.argv.slice(2).length) {
-    program.outputHelp()
+  program.outputHelp();
 }
